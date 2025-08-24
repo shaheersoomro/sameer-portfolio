a2d2f7bca4ca6af2024ac15d8f32e93a2d8fc031
shaheersoomro <rehman.jawed1@gmail.com>
2025-08-24 15:04:29 +0500

    update

--- main.js
+++ main.js
@@ -278,3 +278,17 @@ function initCustomCursor() {
 document.addEventListener("DOMContentLoaded", function () {
   initCustomCursor();
 });
+
+// Throttle function for better performance
+function throttle(func, limit) {
+  let inThrottle;
+  return function() {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
+    }
+  }
+}
